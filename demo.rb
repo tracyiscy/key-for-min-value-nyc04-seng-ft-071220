c403ebfe8a4c7897be598e11e32a0f3008209f7e
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-07-21 16:34:47 +0000

    Automatically backed up by Learn

--- demo.rb
+++ demo.rb
@@ -1,14 +1,19 @@
-def find_min(array)
-  i=0
-  min=array[0]
-  while i<array.length
-    if min>array[i]
-      min=array[i]
+# prereqs: iterators, hashes, conditional logic
+# Given a hash with numeric values, return the key for the smallest value
+
+def key_for_min_value(name_hash)
+  name_hash.each{|key,val|
+    array=Array.new
+    array << val
+    min=array[0]
+    for i in array
+      if min>i
+        min=i
+      end
     end
-    i+=1
-  end
-  puts min
+    puts min
+  }
 end
 
-array=[1,2,-23,-10,5]
-find_min(array)
+hash = {:blake => 500, :ashley => 2, :adam => 1}
+key_for_min_value(hash)
