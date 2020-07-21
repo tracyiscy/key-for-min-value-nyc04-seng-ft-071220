d160010a0429a4d1c72b717b2ab79bd5c3a951b2
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-07-21 16:42:53 +0000

    Done.

--- demo.rb
+++ demo.rb
@@ -2,13 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  smallest_key=nil
-  smallest_value=nil
   name_hash.each{|key,val|
-    if smallest_value==nil || val<smallest_value
-      smallest_value=val
-      smallest_key=key
-    end
+    array=[]
+    array.push(val)
   }
-  smallest_key
+      p array
 end
+
+hash = {:blake => 500, :ashley => 2, :adam => 1}
+key_for_min_value(hash)
